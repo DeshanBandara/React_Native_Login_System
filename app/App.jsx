984b9132fe095ee35a7894f470c94bb91f38867e
DeshanBandara <sudheeradeshan1013@gmail.com>
2024-12-14 02:51:28 +0530

    Navigate the LoginPage into App.jsx

--- app/App.jsx
+++ app/App.jsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import LoginPage from '../components/LoginPage'
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <LoginPage/>
     </View>
   )
 }
